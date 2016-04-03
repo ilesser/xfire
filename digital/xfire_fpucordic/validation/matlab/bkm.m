59a5d1ab4d35b63aba91df9e0cd9125dba4edc79
ilesser <nachoyo89@gmail.com>
2016-04-02 22:49:39 -0300

    vectorized function

--- digital/xfire_fpucordic/validation/matlab/bkm.m
+++ digital/xfire_fpucordic/validation/matlab/bkm.m
@@ -1,4 +1,4 @@
-function [E,L,err]=bkm(E1,L1,bkm_mode,N)
+function [E,L,err]=bkm(E1, L1, bkm_mode = 'E', N = 64)
 % Runs the N steps BKM algorithm for the specified mode with inputs E1 and L1.
 %
 % These are the steps:
@@ -32,121 +32,101 @@ function [E,L,err]=bkm(E1,L1,bkm_mode,N)
 %     L_{N+1} --> L1 + ln(E1)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-   % Calculate ln() and atan() LUTs
-   %n = (1:N-1)';
-   %D = 9;
-   %d  = [0 1 1+j j -1+j -1 -1-j -j 1-j]';
-   %dx = real(d);
-   %dy = imag(d);
-
-   %ln_lut   = (1/2) * log( 1 + 2.^(-n+1) * dx +  2.^(-2*n) * (dx.^2 + dy.^2) );
-   %
-   %atan_lut = zeros(N,9);
-   %
-   %for i=1:D;
-   %   atan_lut(:,i) = dy(i) * atan( 2.^(-n) ./ (1 + dx(i) * 2.^(-n) ) );
-   %endfor
-
    % Initialize variables
-   if ( strcmp(bkm_mode,"E") || strcmp(bkm_mode,"E-mode") )
-      bkm_mode = "E";
-      K  = length(L1);
-      %E1 = ones(1,K) * E1;
-   end
-   if ( strcmp(bkm_mode,"L") || strcmp(bkm_mode,"L-mode") )
-      bkm_mode = "L";
-      K  = length(E1);
-      %L1 = ones(1,K) * L1;
+   if ( strcmp(bkm_mode,'E') || strcmp(bkm_mode,'E-mode') )
+      bkm_mode = 'E';
+   elseif ( strcmp(bkm_mode,'L') || strcmp(bkm_mode,'L-mode') )
+      bkm_mode = 'L';
+   else
+      display 'Error: Unrecognized bkm_mode argument.'
+      exit
    end
 
-   E = zeros(N+1,K);
-   L = zeros(N+1,K);
+   % Find out E1 and L1 lengths
+   KE = length(E1);
+   KL = length(L1);
 
-   E(1,:) = E1;
-   L(1,:) = L1;
+   % EE1 and LL1 are size KLxKE
+   [EE1, LL1] = meshgrid(E1, L1);
+
+   % E and L are size KLxKExN+1
+   E = zeros(KL,KE,N+1);
+   L = zeros(KL,KE,N+1);
+
+   E(:,:,1) = EE1;
+   L(:,:,1) = LL1;
+
 
    % Iterate on n
    for n=(1:N);
 
-      d_n      = get_d_n( n, E(n,:), L(n,:), bkm_mode );
+      d_n         = get_d_n( n, E(:,:,n), L(:,:,n), bkm_mode );
 
-      E(n+1,:)   = E(n,:) .*   (1 + d_n * 2^-n);
-      L(n+1,:)   = L(n,:) - log(1 + d_n * 2^-n);
+      E(:,:,n+1)  = E(:,:,n) .*   (1 + d_n * 2^-n);
+      L(:,:,n+1)  = L(:,:,n) - log(1 + d_n * 2^-n);
 
    endfor
 
-
-   E_ideal = E1.* exp(L1);
-   L_ideal = L1 + log(E1);
-
-   err_E   = ( E(N+1,:) - E_ideal ) ./ E_ideal;
-   err_L   = ( L(N+1,:) - L_ideal ) ./ L_ideal;
-
-   if ( bkm_mode == "E" )
-      err = err_E;
-   elseif ( bkm_mode == "L" )
-      err = err_L;
+   % Calculate error vs ideal value
+   if ( bkm_mode == 'E' )
+      E_ideal  = EE1.* exp(LL1);
+      %err     = ( E(:,:,N+1) - E_ideal ) ./ E_ideal;
+      %err     = ( E(:,:,N+1) - E_ideal ) ;
+      err      = ( E - E_ideal ) ;
+   elseif ( bkm_mode == 'L' )
+      L_ideal  = LL1 + log(EE1);
+      %err     = ( L(:,:,N+1) - L_ideal ) ./ L_ideal;
+      %err     = ( L(:,:,N+1) - L_ideal ) ;
+      err      = ( L - L_ideal ) ;
    else
       err = -1;
    end
+
 endfunction
-
-
-
-
 
 function dn = get_d_n( n, En, Ln, bkm_mode )
 
+   [KL, KE] = size(En);
 
-   if ( bkm_mode == "E" )
+   drn = din = zeros(KL,KE);
 
-      dxn = zeros(1, length(Ln));
-      dyn = zeros(1, length(Ln));
+   if ( bkm_mode == 'E' )
 
       % Truncate 2^n * Lx after the 3rd fractional digit
-      Lxn = fix(real(Ln)*2^n*2^3)/2^3;
+      Lrn = fix(real(Ln)*2^n*2^3)/2^3;
 
       % Truncate 2^n * Ly after the 4th fractional digit
-      Lyn = fix(imag(Ln)*2^n*2^4)/2^4;
+      Lin = fix(imag(Ln)*2^n*2^4)/2^4;
 
-      dxn(Lxn <= -5/8              ) = -1;
-      dxn(Lxn >= -1/2 && Lxn <= 1/4) = 0;
-      dxn(Lxn >=  3/8              ) =  1;
+      drn(Lrn <= -5/8              ) = -1;
+      drn(Lrn >= -1/2 && Lrn <= 1/4) =  0;
+      drn(Lrn >=  3/8              ) = +1;
 
-      dyn(Lyn <= -13/16            ) = -1;
-      dyn(Lyn >= -3/4 && Lyn <= 3/4) = 0;
-      dyn(Lyn >=  13/16            ) =  1;
+      din(Lin <= -13/16            ) = -1;
+      din(Lin >= -3/4 && Lin <= 3/4) =  0;
+      din(Lin >=  13/16            ) = +1;
 
-   elseif ( bkm_mode == "L" )
-
-      dxn = zeros(length(En), 1);
-      dyn = zeros(length(En), 1);
+   elseif ( bkm_mode == 'L' )
 
       % Get the new sequence
-      En  = 2^n * (En-1);
+      En
+      en  = 2^n * (En-1)
 
       % Truncate real and imaginary parts after their 4th fractional digit
-      Exn = fix(real(En)*2^4)/2^4;
-      Eyn = fix(imag(En)*2^4)/2^4;
+      ern = fix(real(en)*2^4)/2^4
+      ein = fix(imag(en)*2^4)/2^4
 
-      if     ( Exn <= -1/2 )
-         dxn = 1;
-      elseif ( Exn > -1/2 && Exn <= 1/2 )
-         dxn = 0;
-      elseif ( Exn > 1/2 )
-         dxn = -1;
-      end
+      drn(ern <= -1/2              ) = +1;
+      drn(ern >  -1/2 && ern <= 1/2) =  0;
+      drn(ern >   1/2              ) = -1;
 
-      if     ( Eyn <= -1/2 )
-         dyn = 1;
-      elseif ( Eyn > -1/2 && Eyn <= 1/2 )
-         dyn = 0;
-      elseif ( Eyn > 1/2 )
-         dyn = -1;
-      end
+      din(ein <= -1/2              ) = +1;
+      din(ein >  -1/2 && ein <= 1/2) =  0;
+      din(ein >   1/2              ) = -1;
 
    end
 
-   dn = dxn + j * dyn;
+   dn = drn + j * din
+   display ''
 
 end
