af1a9f5e182f55305c08827b5a4c92b2215a0107
ilesser <ilesser@users.noreply.github.com>
2016-04-04 19:00:07 -0300

    Connected BKM Pre, Core and Post to control logic.

--- digital/xfire_fpucordic/rtl/source/bkm_fixed.v
+++ digital/xfire_fpucordic/rtl/source/bkm_fixed.v
@@ -50,6 +50,7 @@
 // History:
 // --------
 //
+//    - 2016-04-04 - ilesser - Connected BKM Pre, Core and Post to Control Logic.
 //    - 2016-04-04 - ilesser - Original version.
 //
 // -----------------------------------------------------------------------------
@@ -88,8 +89,8 @@ module bkm_fixed #(
     // ----------------------------------
     // Data outputs
     // ----------------------------------
-    input reg  [W-1:0]        xo,
-    input reg  [W-1:0]        yo,
+    input reg  [W-1:0]        x3,
+    input reg  [W-1:0]        y3,
     output wire[FLO_FSIZE-1:0]flags,
     output wire               done
   );
@@ -102,8 +103,38 @@ module bkm_fixed #(
    // -----------------------------------------------------
    // Internal signals
    // -----------------------------------------------------
-   wire [XXXXX-1:0]  XXXXX;
-   reg  [XXXXX-1:0]  XXXXX;
+   // BKM Pre control wires
+   wire              bkm_pre_enable;
+   wire              bkm_pre_start;
+   wire  [1:0]       bkm_pre_format;
+   wire  [OPSIZE-1:0]bkm_pre_op;
+   wire              bkm_pre_done;
+
+   // BKM Core control wires
+   wire              bkm_core_enable;
+   wire              bkm_core_start;
+   wire  [1:0]       bkm_core_format;
+   wire              bkm_core_mode;
+   wire              bkm_core_done;
+   wire  [F-1:0]     bkm_core_flags;
+
+   // BKM Core Signal Path
+   wire  [W-1:0]     E_r_in
+   wire  [W-1:0]     E_i_in
+   wire  [W-1:0]     L_r_in
+   wire  [W-1:0]     L_i_in
+   wire  [W-1:0]     E_r_out
+   wire  [W-1:0]     E_i_out
+   wire  [W-1:0]     L_r_out
+   wire  [W-1:0]     L_i_out
+
+   // BKM Post control wires
+   wire              bkm_post_enable;
+   wire              bkm_post_start;
+   wire  [1:0]       bkm_post_format;
+   wire              bkm_post_op;
+   wire              bkm_post_done;
+   wire  [F-1:0]     bkm_post_flags;
    // -----------------------------------------------------
 
    // -----------------------------------------------------
@@ -113,11 +144,8 @@ module bkm_fixed #(
     // ----------------------------------
     // Parameters
     // ----------------------------------
-      .W                   (W),
-      .LOG2W               (LOG2W),
-      .N                   (N),
-      .LOG2N               (LOG2N)
-   ) bkm_post_processign (
+      .W                   (W)
+   ) bkm_pre_processign (
     // ----------------------------------
     // Clock, reset & enable inputs
     // ----------------------------------
@@ -131,17 +159,19 @@ module bkm_fixed #(
       .start               (bkm_pre_start),
       .format              (bkm_pre_format),
       .op                  (bkm_pre_op),
-      .E_r_in              (E_r_pre),
-      .E_i_in              (E_i_pre),
-      .L_r_in              (L_r_pre),
-      .L_i_in              (L_i_pre),
+      .x1                  (x1),
+      .y1                  (y1),
+      .x2                  (x2),
+      .y2                  (y2),
+      .x3                  (x3),
+      .y3                  (y3),
     // ----------------------------------
     // Data outputs
     // ----------------------------------
-      .E_r_out             (E_r_0),
-      .E_i_out             (E_i_0),
-      .L_r_out             (L_r_0),
-      .L_i_out             (L_i_0),
+      .E_r                 (E_r_in),
+      .E_i                 (E_i_in),
+      .L_r                 (L_r_in),
+      .L_i                 (L_i_in),
       .done                (bkm_pre_done)
    );
    // -----------------------------------------------------
@@ -171,17 +201,17 @@ module bkm_fixed #(
       .start               (bkm_core_start),
       .mode                (bkm_core_mode),
       .format              (bkm_core_format),
-      .E_r_in              (E_r_0),
-      .E_i_in              (E_i_0),
-      .L_r_in              (L_r_0),
-      .L_i_in              (L_i_0),
+      .E_r_in              (E_r_in),
+      .E_i_in              (E_i_in),
+      .L_r_in              (L_r_in),
+      .L_i_in              (L_i_in),
     // ----------------------------------
     // Data outputs
     // ----------------------------------
-      .E_r_out             (E_r_Np1),
-      .E_i_out             (E_i_Np1),
-      .L_r_out             (L_r_Np1),
-      .L_i_out             (L_i_Np1),
+      .E_r_out             (E_r_out),
+      .E_i_out             (E_i_out),
+      .L_r_out             (L_r_out),
+      .L_i_out             (L_i_out),
       .flags               (bkm_core_flags),
       .done                (bkm_core_done)
    );
@@ -194,10 +224,7 @@ module bkm_fixed #(
     // ----------------------------------
     // Parameters
     // ----------------------------------
-      .W                   (W),
-      .LOG2W               (LOG2W),
-      .N                   (N),
-      .LOG2N               (LOG2N)
+      .W                   (W)
    ) bkm_post_processign (
     // ----------------------------------
     // Clock, reset & enable inputs
@@ -219,20 +246,73 @@ module bkm_fixed #(
     // ----------------------------------
     // Data outputs
     // ----------------------------------
-      .x                   (E_r_post),
-      .y                   (E_i_post),
+      .x                   (x3),
+      .y                   (y3),
       .flags               (bkm_post_flags),
       .done                (bkm_post_done)
    );
-
-
    // -----------------------------------------------------
 
    // -----------------------------------------------------
-   // Control logic
+   // BKM Fixed Control Logic
    // -----------------------------------------------------
+   bkm_control_logic #(
+    // ----------------------------------
+    // Parameters
+    // ----------------------------------
+      .W                   (W)
+   ) bkm_control_logic (
+    // ----------------------------------
+    // Clock, reset & enable inputs
+    // ----------------------------------
+      .clk                 (clk),
+      .arst                (arst),
+      .srst                (srst),
+      .enable              (enable),
+    // ----------------------------------
+    // Data inputs
+    // ----------------------------------
+      // From outer world
+      .start               (start),
+      .format              (format),
+      .op                  (op),
 
+      // From BKM Pre
+      .bkm_pre_done        (bkm_pre_done),
 
+      // From BKM Core
+      .bkm_core_done       (bkm_core_done),
+      .bkm_core_flags      (bkm_core_flags),
+
+      // From BKM Post
+      .bkm_post_done       (bkm_post_done),
+      .bkm_post_flags      (bkm_post_flags),
+
+    // ----------------------------------
+    // Data outputs
+    // ----------------------------------
+      // To BKM Pre
+      .bkm_pre_enable      (bkm_pre_enable),
+      .bkm_pre_start       (bkm_pre_start),
+      .bkm_pre_format      (bkm_pre_format),
+      .bkm_pre_op          (bkm_pre_op),
+
+      // To BKM Core
+      .bkm_core_enable      (bkm_core_enable),
+      .bkm_core_start       (bkm_core_start),
+      .bkm_core_format      (bkm_core_format),
+      .bkm_core_mode        (bkm_core_mode),
+
+      // To BKM Post
+      .bkm_post_enable      (bkm_post_enable),
+      .bkm_post_start       (bkm_post_start),
+      .bkm_post_format      (bkm_post_format),
+      .bkm_post_op          (bkm_post_op),
+
+      // To outer world
+      .flags               (flags),
+      .done                (done)
+   );
    // -----------------------------------------------------
 
 
