47627898f9bb5f7ce8f4c2b449b6a87c4ac6cc7b
ilesser <nachoyo89@gmail.com>
2016-04-16 21:38:49 -0300

    Implemented paper 'Efficient canonic signed digit recording'

--- digital/xfire_fpu_bkm/verification/bin2csd/tests/basic_test.v
+++ digital/xfire_fpu_bkm/verification/bin2csd/tests/basic_test.v
@@ -45,22 +45,55 @@ task basic_test;
 
    begin
 
-      $monitor ("Time = %8t tb_x = %b \twire_y = %b B = %b\n\t\t\t\tres    = %b\n\n",$time, tb_x, wire_y, duv.B, res);
-      //$dumpfile("tb_bin2csd.vcd");
-      //$dumpvars(:,   tb_bin2csd);
-      //$dumpvars(0,   tb_bin2csd);
-      //$dumpvars(1,   duv.cxB,
-      //               duv.cyB,
-      //               duv.sxB,
-      //               duv.syB);
-      #1    tb_x = 4'b0000;   res = 8'b00000000;
-      #1    tb_x = 4'b0001;   res = 8'b00000001;
-      #1    tb_x = 4'b0010;   res = 8'b00000100;
-      #1    tb_x = 4'b0100;   res = 8'b00010000;
-      #1    tb_x = 4'b1000;   res = 8'b01000000;
-      #1    tb_x = 4'b0110;   res = 8'b01001100;
-      #1    tb_x = 4'b0111;   res = 8'b01000011;
-      //#10   $finish;     //finish after 20 time units
+      $monitor ("Time = %8t tb_x = %b \twire_y = %b \n\t\t\t\tres    = %b\n\n",$time, tb_x, wire_y, res);
+      $dumpfile("../waves/tb_bin2csd_basic_test.vcd");
+      $dumpvars();
+      load_operands(5'b00000, 10'b0000000000);  // res = 0
+      load_operands(5'b00001, 10'b0000000001);  // res = 1
+      load_operands(5'b00010, 10'b0000000100);  // res = 2
+      load_operands(5'b00100, 10'b0000010000);  // res = 4
+      load_operands(5'b01000, 10'b0001000000);  // res = 8
+      load_operands(5'b10000, 10'b1000000000);  // res =-16
+      load_operands(5'b00110, 10'b0001001000);  // res = 6 = 8-2
+      load_operands(5'b00111, 10'b0001000010);  // res = 7 = 8-1
+      load_operands(5'b10111, 10'b0010000010);  // res =-9 =-8-1
+      load_operands(5'b11111, 10'b0000000010);  // res = 1
+      load_operands(5'b00011, 10'b0000010010);  // res = 3 =-4+1
+      load_operands(5'b01011, 10'b0100100010);  // res = 11 = 16-4-1
+
+   end
+
+// *****************************************************************************
+
+endtask
+
+// *****************************************************************************
+// Interface
+// *****************************************************************************
+task load_operands;
+// *****************************************************************************
+
+// *****************************************************************************
+// Functionality
+// *****************************************************************************
+
+   // -----------------------------------------------------
+   // Internal variables and signals
+   // -----------------------------------------------------
+   input [W-1:0]     x;
+   input [2*W-1:0]   y;
+   // -----------------------------------------------------
+
+   begin
+
+      tb_x  = x;
+      res   = y;
+      #1
+
+      if (wire_y != y) begin
+         `ERR_MSG2(\tExpected result: %b\n\t\tObtained result: %b\t\t, y, wire_y);
+         $display("");
+      end
 
    end
 
