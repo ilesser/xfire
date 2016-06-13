14c98cef10f14f24b10716f24d91b508f2760e3c
Ignacio Lesser <ilesser@users.noreply.github.com>
2016-06-13 15:25:13 -0300

    Added sequential test for csd_add_subb. FAILING!

--- digital/xfire_fpu_bkm/verification/csd_add_subb/tb/tb_csd_add_subb.v
+++ digital/xfire_fpu_bkm/verification/csd_add_subb/tb/tb_csd_add_subb.v
@@ -30,7 +30,10 @@
 
 `define SIM_CLK_PERIOD_NS 10
 `timescale 1ns/1ps
+`define W 4
+`define CNT_SIZE (2*`W)+2
 
+`include "/home/ilesser/simlib/simlib_defs.vh"
 // *****************************************************************************
 // Interface
 // *****************************************************************************
@@ -44,28 +47,55 @@ module tb_csd_add_subb ();
    // -----------------------------------------------------
    // Testbench controlled variables and signals
    // -----------------------------------------------------
-   localparam        W = 4;
-   reg               tb_subb_x;
-   reg               tb_subb_y;
-   reg   [W-1:0]     tb_x_bin;
-   reg   [W-1:0]     tb_y_bin;
-   reg   [W-1:0]     tb_z_bin;
+   localparam              W = `W;
+   localparam              CNT_SIZE = `CNT_SIZE;
+   reg                     clk, rst, ena;
+   reg                     tb_subb_x;
+   reg                     tb_subb_y;
+   reg   [W-1:0]           tb_x_bin;
+   reg   [W-1:0]           tb_y_bin;
+   reg   [W-1:0]           tb_z_bin;
+   reg                     tb_c_bin;
+   reg   [`CNT_SIZE-1:0]   cnt;
    // -----------------------------------------------------
 
    // -----------------------------------------------------
    // Testbecnch wiring
    // -----------------------------------------------------
-   wire              c_bin;
-   wire  [W-1:0]     z_bin;
-   wire  [2*W-1:0]   x_csd;
-   wire  [2*W-1:0]   y_csd;
-   wire  [1:0]       c_csd;
-   wire  [2*W-1:0]   z_csd;
+   wire                    c_bin;
+   wire  [W-1:0]           z_bin;
+   wire  [2*W-1:0]         x_csd;
+   wire  [2*W-1:0]         y_csd;
+   wire  [1:0]             c_csd;
+   wire  [2*W-1:0]         z_csd;
    // -----------------------------------------------------
 
    // -----------------------------------------------------
    // Transactors
    // -----------------------------------------------------
+   simlib_clk_osc #(
+      // ----------------------------------------------
+      // Parameters
+      // ----------------------------------------------
+      .CLK_PERIOD_NS    (`SIM_CLK_PERIOD_NS)
+   ) clk_osc (
+      // ----------------------------------------------
+      // Ports in
+      // ----------------------------------------------
+      .stop             (1'b0),
+      // ----------------------------------------------
+      // Ports out
+      // ----------------------------------------------
+      .clk_out          (clk)
+   );
+
+   always @(posedge clk)
+       if (rst) begin
+          cnt <= {CNT_SIZE{1'b0}};
+       end else if (ena) begin
+          cnt <= cnt + 1;
+       end
+
    bin2csd #(
     // ----------------------------------
     // Parameters
@@ -119,11 +149,40 @@ module tb_csd_add_subb ();
    // -----------------------------------------------------
    // Monitors
    // -----------------------------------------------------
+   initial begin
+      $monitor("Time = %8t tb_subb_x = %b tb_subb_x = %b tb_x_bin = %6d tb_y_bin = %6d tb_z_bin = %b %6d z_bin = %b %6d\n",$time, tb_subb_x, tb_subb_y, tb_x_bin, tb_y_bin, tb_c_bin, tb_z_bin, c_bin, z_bin);
+      $dumpfile("../waves/tb_csd_add_subb.vcd");
+      $dumpvars();
+   end
    // -----------------------------------------------------
 
    // -----------------------------------------------------
    // Checkers
    // -----------------------------------------------------
+   always @(posedge clk) begin
+      if (ena == 1'b1) begin
+         if (tb_z_bin != z_bin) begin
+            $display("\t\t\t\t\t\t    %b", tb_x_bin);
+            $display("\t\t\t\t\t\t  + %b", tb_y_bin);
+            $display("\t\t\t\t\t\t --------");
+            $display("[%0d] ERROR: Different sum!\tExpected result: %b %b\n\t\t\t\tObtained result: %b %b\t\t. Instance: %m",$time, tb_c_bin, tb_z_bin, c_bin, z_bin);
+            add_error();
+            finish_sim();
+         end
+      end
+   end
+
+   always @(posedge clk) begin
+      if (ena == 1'b1) begin
+         if (tb_c_bin != c_bin) begin
+            $display("\t\t\t\t\t\t    %b", tb_x_bin);
+            $display("\t\t\t\t\t\t  + %b", tb_y_bin);
+            $display("\t\t\t\t\t\t --------");
+            $display("[%0d] WARNING: Different carry! Expected result: %b %b\n\t\t\t\t Obtained result: %b %b\t\t. Instance: %m",$time, tb_c_bin, tb_z_bin, c_bin, z_bin);
+            add_warning();
+         end
+      end
+   end
    // -----------------------------------------------------
 
    // -----------------------------------------------------
