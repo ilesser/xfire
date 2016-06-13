f5a6b1b32ae5a96f6a8d2df2b6a965bd8fd6bec5
Ignacio Lesser <ilesser@users.noreply.github.com>
2016-06-13 12:30:52 -0300

    added sequential test to csd2bin. It passes!

--- digital/xfire_fpu_bkm/verification/csd2bin/tb/tb_csd2bin.v
+++ digital/xfire_fpu_bkm/verification/csd2bin/tb/tb_csd2bin.v
@@ -24,12 +24,22 @@
 // History:
 // --------
 //
+//    - 2016-06-13 - ilesser - Added sequential test.
 //    - 2016-04-18 - ilesser - Original version.
 //
 // -----------------------------------------------------------------------------
 
 `define SIM_CLK_PERIOD_NS 10
 `timescale 1ns/1ps
+`define W 5
+`define W2 `W*2
+`define CNT_SIZE `W
+
+`define CSD_0  2'b00
+`define CSD_p1 2'b01
+`define CSD_m1 2'b10
+
+`include "/home/ilesser/simlib/simlib_defs.vh"
 
 // *****************************************************************************
 // Interface
@@ -44,9 +54,12 @@ module tb_csd2bin ();
    // -----------------------------------------------------
    // Testbench controlled variables and signals
    // -----------------------------------------------------
-   localparam        W=5;
-   reg   [2*W-1:0]   tb_x;
-   reg   [W-1:0]     res;
+   localparam              W=5;
+   localparam              CNT_SIZE = `CNT_SIZE;
+   reg                     clk, rst, ena;
+   reg   [2*W-1:0]         tb_x;
+   reg   [W-1:0]           res;
+   reg   [CNT_SIZE-1:0]    cnt;
    // -----------------------------------------------------
 
    // -----------------------------------------------------
@@ -58,16 +71,51 @@ module tb_csd2bin ();
    // -----------------------------------------------------
    // Transactors
    // -----------------------------------------------------
+   simlib_clk_osc #(
+      // ----------------------------------------------
+      // Parameters
+      // ----------------------------------------------
+      .CLK_PERIOD_NS    (`SIM_CLK_PERIOD_NS)
+   ) clk_osc (
+      // ----------------------------------------------
+      // Ports in
+      // ----------------------------------------------
+      .stop             (1'b0),
+      // ----------------------------------------------
+      // Ports out
+      // ----------------------------------------------
+      .clk_out          (clk)
+   );
+
+   always @(posedge clk)
+       if (rst) begin
+          cnt <= {CNT_SIZE{1'b0}} ;
+       end else if (ena) begin
+          cnt <= cnt + 1;
+       end
    // -----------------------------------------------------
 
    // -----------------------------------------------------
    // Monitors
    // -----------------------------------------------------
+   initial begin
+      $monitor("Time = %8t tb_x = %b\twire_y =\t%b\n\t\t\t\t\tres =\t\t%b\n\n",$time, tb_x, wire_y, res);
+      $dumpfile("../waves/tb_csd2bin.vcd");
+      $dumpvars();
+   end
    // -----------------------------------------------------
 
    // -----------------------------------------------------
    // Checkers
    // -----------------------------------------------------
+   always @(posedge clk) begin
+      if (ena == 1'b1) begin
+         if (res != wire_y) begin
+            $display("[%0d] ERROR: Different conversion!\tExpected result:\t%b\n\t\t\t\t\tObtained result:\t%b. Instance: %m",$time, res, wire_y);
+            //$finish();
+         end
+      end
+   end
    // -----------------------------------------------------
 
    // -----------------------------------------------------
