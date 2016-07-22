1a4d5a6f6974d377ff5c5a15c50a585dcc6347ba
Ignacio Lesser <nachoyo89@gmail.com>
2016-07-22 09:43:20 -0300

    WIP: adding bkm_data_step and bkm_control_step

--- digital/xfire_fpu_bkm/verification/bkm_data_step/tb/tb_bkm_data_step.v
+++ digital/xfire_fpu_bkm/verification/bkm_data_step/tb/tb_bkm_data_step.v
@@ -12,19 +12,19 @@
 // Description:
 // ------------
 //
-// Testbench for bkm_step block.
+// Testbench for bkm_data_step block.
 //
 // -----------------------------------------------------------------------------
 // File name:
 // ----------
 //
-// tb_bkm_step.v
+// tb_bkm_data_step.v
 //
 // -----------------------------------------------------------------------------
 // History:
 // --------
 //
-//    - 2016-07-06 - ilesser - Initial version.
+//    - 2016-07-23 - ilesser - Initial version.
 //
 // -----------------------------------------------------------------------------
 
@@ -41,7 +41,7 @@
 // *****************************************************************************
 // Interface
 // *****************************************************************************
-module tb_bkm_step ();
+module tb_bkm_data_step ();
 // *****************************************************************************
 
 // *****************************************************************************
@@ -67,10 +67,6 @@ module tb_bkm_step ();
    reg   [W-1:0]           tb_lut_X,   tb_lut_Y;
    reg   [W-1:0]           tb_X_np1,   tb_Y_np1;
    reg   [W-1:0]           delta_X,    delta_Y;
-   reg   [W/4-1:0]         tb_u_n,     tb_v_n;
-   reg   [W/4-1:0]         tb_lut_u,   tb_lut_v;
-   reg   [W/4-1:0]         tb_u_np1,   tb_v_np1;
-   reg   [W/4-1:0]         delta_u,    delta_v;
    reg   [`CNT_SIZE-1:0]   cnt;
    // -----------------------------------------------------
 
@@ -81,7 +77,6 @@ module tb_bkm_step ();
    wire  [2*W-1:0]         X_np1_csd,  Y_np1_csd;
    wire  [2*W-1:0]         lut_X_csd,  lut_Y_csd;
    wire  [W-1:0]           res_X_np1,  res_Y_np1;
-   wire  [W/4-1:0]         res_u_np1,  res_v_np1;
    // -----------------------------------------------------
 
    // -----------------------------------------------------
@@ -218,17 +213,44 @@ module tb_bkm_step ();
                "\ttb_d_x_n=%b\ttb_d_y_n=%b\n",                             tb_d_x_n, tb_d_y_n,
                "\ttb_X_n=%6d\ttb_Y_n=%6d\t tb_X_np1=%6d\t tb_Y_np1=%6d\n", tb_X_n, tb_Y_n, tb_X_np1, tb_Y_np1,
                "\t\t\t\t\tres_X_np1=%6d\tres_Y_np1=%6d\n",                                res_X_np1,res_Y_np1,
-               "\ttb_u_n=%6d\ttb_v_n=%6d\t tb_u_np1=%6d\t tb_v_np1=%6d\n", tb_u_n, tb_v_n, tb_u_np1, tb_v_np1,
-               "\t\t\t\t\tres_u_np1=%6d\tres_v_np1=%6d\n",                                res_u_np1,res_v_np1,
             );
 
-      //$dumpfile("../waves/tb_bkm_step.vcd");
+      //$dumpfile("../waves/tb_bkm_data_step.vcd");
       //$dumpvars();
    end
    // -----------------------------------------------------
 
    // -----------------------------------------------------
    // Checkers
+   // -----------------------------------------------------
+
+   bkm_data_step_checker #(
+      .W          (`W),
+      .LOG2W      (`LOG2W),
+      .LOG2N      (`LOG2N)
+   ) checker (
+      // ----------------------------------
+      // Clock, reset & enable inputs
+      // ----------------------------------
+      .clk        (clk),
+      .arst       (arst),
+      .srst       (srst),
+      .enable     (ena),
+      // ----------------------------------
+      // Data inputs
+      // ----------------------------------
+      .tb_X_np1   (tb_X_np1),
+      .tb_Y_np1   (tb_Y_np1),
+      .res_X_np1  (res_X_np1),
+      .res_Y_np1  (res_Y_np1),
+      // ----------------------------------
+      // Data outputs
+      // ----------------------------------
+      .err_X      (err_X),
+      .err_Y      (err_Y),
+      .delta_X    (delta_X),
+      .delta_Y    (delta_Y)
+   );
    // -----------------------------------------------------
    always @(posedge clk) begin
       if (srst == 1'b1) begin
@@ -273,57 +295,12 @@ module tb_bkm_step ();
          end
       end
    end
-
-   always @(posedge clk) begin
-      if (srst == 1'b1) begin
-         err_u    <= 1'b0;
-         delta_u  <= {W{1'b0}};
-      end
-      else begin
-         if (ena == 1'b1) begin
-            if (tb_u_np1 !== res_u_np1) begin
-               $display("[%0d] ERROR: in u.\tExpected result: %d\n\t\t\tObtained result: %d\t\t. Instance: %m",$time, tb_u_np1, res_u_np1);
-               add_error();
-               //finish_sim();
-               err_u    <= 1'b1;
-               delta_u  <= tb_u_np1 - res_u_np1;
-            end
-            else begin
-               add_note();
-               err_u    <= 1'b0;
-            end
-         end
-      end
-   end
-
-   always @(posedge clk) begin
-      if (srst == 1'b1) begin
-         err_v    <= 1'b0;
-         delta_v  <= {W{1'b0}};
-      end
-      else begin
-         if (ena == 1'b1) begin
-            if (tb_v_np1 !== res_v_np1) begin
-               $display("[%0d] ERROR: in v.\tExpected result: %d\n\t\t\tObtained result: %d\t\t. Instance: %m",$time, tb_v_np1, res_v_np1);
-               add_error();
-               //finish_sim();
-               err_v    <= 1'b1;
-               delta_v  <= tb_v_np1 - res_v_np1;
-            end
-            else begin
-               add_note();
-               err_v    <= 1'b0;
-            end
-         end
-      end
-   end
-
    // -----------------------------------------------------
 
    // -----------------------------------------------------
    // Device under verifiacion
    // -----------------------------------------------------
-   bkm_step #(
+   bkm_data_step #(
       .W          (`W),
       .LOG2W      (`LOG2W),
       .LOG2N      (`LOG2N)
@@ -347,17 +324,11 @@ module tb_bkm_step ();
       .Y_n        (Y_n_csd),
       .lut_X      (lut_X_csd),
       .lut_Y      (lut_Y_csd),
-      .u_n        (tb_u_n),
-      .v_n        (tb_v_n),
-      .lut_u      (tb_lut_u),
-      .lut_v      (tb_lut_v),
       // ----------------------------------
       // Data outputs
       // ----------------------------------
       .X_np1      (X_np1_csd),
-      .Y_np1      (Y_np1_csd),
-      .u_np1      (res_u_np1),
-      .v_np1      (res_v_np1)
+      .Y_np1      (Y_np1_csd)
    );
    // -----------------------------------------------------
 
