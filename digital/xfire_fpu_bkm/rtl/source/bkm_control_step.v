b9e6bcb63b388450a825efa3786b386476858dde
Ignacio Lesser <ilesser@users.noreply.github.com>
2016-07-22 09:43:20 -0300

    WIP: adding bkm_data_step and bkm_control_step

--- digital/xfire_fpu_bkm/rtl/source/bkm_control_step.v
+++ digital/xfire_fpu_bkm/rtl/source/bkm_control_step.v
@@ -4,20 +4,13 @@
 // Description:
 // ------------
 //
-// Performs the nth step of the BKM algorithm.
+// Performs the nth control step of the BKM algorithm.
 //
 // E-mode
 // ------
-// Z = E
 // w = 2^1 * L
-// Z_{n+1} = Z_n * (1 + d_n * 2^-n)
 // w_{n+1} = 2 w_n - 2^(n+1) * ln(1 + d_n * 2^-n)
 //
-//                          -n
-// Z     =  Z  * (1 + d  * 2   )
-//  n+1      n         n
-//
-//               _                              _
 //              |        n                  -n   |
 // w     =  2 * |  w  - 2   *  ln(1 + d  * 2   ) |
 //  n+1         |_  n                  n        _|
@@ -27,14 +20,8 @@
 //
 // L-mode
 // ------
-// Z = L
 // w = 2^1 * (E-1)
-// Z_{n+1} = Z_n - ln(1 + d_n * 2^-n)
 // w_{n+1} = 2 * [ w_n + d_n + d_n * w_n * 2^-n ]
-//
-//                           -n
-// Z     = Z  - ln(1 + d  * 2   )
-//  n+1     n           n
 //
 //              _                           _
 //             |                 -n          |
@@ -51,7 +38,7 @@
 // File name:
 // ----------
 //
-// bkm_step.v
+// bkm_control_step.v
 //
 // -----------------------------------------------------------------------------
 // Interface:
@@ -68,14 +55,10 @@
 //    - format    : Format code (logic, 2 bits).   FF
 //                                                 ||--> Precision:  0 for 64 bit, 1 for 32 bit
 //                                                 |---> Complex:    0 for complex args, 1 for real args
-//    - X_in      : Real      part of the data input (two's complement, W bits).
-//    - Y_in      : Imaginary part of the data input (two's complement, W bits).
 //    - u_in      : Real      part of the control input (two's complement, W bits).
 //    - v_in      : Imaginary part of the control input (two's complement, W bits).
 //
 //  Data outputs:
-//    - X_out     : Real      part of the result (two's complement, W bits).
-//    - Y_out     : Imaginary part of the result (two's complement, W bits).
 //    - flags     : Result flags (logic, 4 bits).
 //    - done      : Active high done strobe signal (logic, 1 bit).
 //
@@ -90,11 +73,7 @@
 // History:
 // --------
 //
-//    - 2016-07-23 - ilesser - Changed architecture: created bkm_control_step and bkm_data_step.
-//    - 2016-07-18 - ilesser - Added CSD barrel shifter.
-//    - 2016-07-18 - ilesser - Renamed csd_* to *_csd.
-//    - 2016-07-18 - ilesser - Removed regs by wires. Signal definition clean up.
-//    - 2016-04-23 - ilesser - Initial version.
+//    - 2016-07-22 - ilesser - Initial version.
 //
 // -----------------------------------------------------------------------------
 
@@ -103,7 +82,7 @@
 // *****************************************************************************
 // Interface
 // *****************************************************************************
-module bkm_step #(
+module bkm_control_step #(
     // ----------------------------------
     // Parameters
     // ----------------------------------
@@ -124,12 +103,8 @@ module bkm_step #(
     input wire                mode,
     input wire [1:0]          format,
     input wire [LOG2N-1:0]    n,
-    input wire [1:0]          d_x_n,        // d_n is encoded in ones complement
-    input wire [1:0]          d_y_n,        // d_n is encoded in ones complement
-    input wire [2*W-1:0]      X_n,
-    input wire [2*W-1:0]      Y_n,
-    input wire [2*W-1:0]      lut_X,
-    input wire [2*W-1:0]      lut_Y,
+    input wire [1:0]          d_u_n,        // d_n is encoded in ones complement
+    input wire [1:0]          d_v_n,        // d_n is encoded in ones complement
     input wire [W/4-1:0]      u_n,
     input wire [W/4-1:0]      v_n,
     input wire [W/4-1:0]      lut_u,
@@ -137,8 +112,6 @@ module bkm_step #(
     // ----------------------------------
     // Data outputs
     // ----------------------------------
-    output reg [2*W-1:0]      X_np1,
-    output reg [2*W-1:0]      Y_np1,
     output reg [W/4-1:0]      u_np1,
     output reg [W/4-1:0]      v_np1
   );
@@ -164,106 +137,167 @@ module bkm_step #(
 //             +--------------------------------+
 //
 //
-//             +--------------------------------+
-//             |                                |
-// n      ---->+                                |
-// d_x_n  ---->+                                |
-// d_y_n  ---->+                                |
-//             |           DATA STEP            |
-// X_n    ---->+                                +---->   X_np1
-// Y_n    ---->+                                +---->   Y_np1
-// lut_X  ---->+                                |
-// lut_Y  ---->+                                |
-//             |                                |
-//             +--------------------------------+
-
 
    // -----------------------------------------------------
    // Internal signals
    // -----------------------------------------------------
-   // For the control path
-   wire  [1:0]          d_u_n;
-   wire  [1:0]          d_v_n;
+   wire                 sign_a_u;
+   wire                 sign_a_v;
+   wire                 sign_b_u;
+   wire                 sign_b_v;
+   wire  [W/4-1:0]      a_u;
+   wire  [W/4-1:0]      a_v;
+   wire  [W/4-1:0]      b_u;
+   wire  [W/4-1:0]      b_v;
+   wire  [W/4-1:0]      u_n_plus_d_u_n;
+   wire  [W/4-1:0]      v_n_plus_d_v_n;
+   wire  [W/4-1:0]      u_n_times_d_u_n;
+   wire  [W/4-1:0]      v_n_times_d_v_n;
+   wire                 c_u;
+   wire                 c_v;
+   wire  [W/4-1:0]      s_u;
+   wire  [W/4-1:0]      s_v;
+   wire  [W/4-1:0]      b_u_shifted;
+   wire  [W/4-1:0]      b_v_shifted;
    // -----------------------------------------------------
 
-   assign d_u_n = d_x_n;
-   assign d_v_n = d_y_n;
+// *****************************************************************************
+// CONTROL PATH
+// *****************************************************************************
 
    // -----------------------------------------------------
-   // Control step
+   // Multiply by d_n
    // -----------------------------------------------------
-   bkm_control_step #(
+   multiply_by_d #(
     // ----------------------------------
     // Parameters
     // ----------------------------------
-      .W                   (W),
-      .LOG2W               (LOG2W),
-      .LOG2N               (LOG2N)
-   ) bkm_control_step (
-   // ----------------------------------
-   // Clock, reset & enable inputs
-   // ----------------------------------
-      .clk                 (clk),
-      .arst                (arst),
-      .srst                (srst),
-      .enable              (enable),
+      .W                   (W/4)
+   ) multiply_by_d (
     // ----------------------------------
     // Data inputs
     // ----------------------------------
-      .mode                (mode),
-      .format              (format),
-      .n                   (n),
-      .d_u_n               (d_u_n),
-      .d_v_n               (d_v_n),
-      .u_n                 (u_n),
-      .v_n                 (v_n),
-      .lut_u               (lut_u),
-      .lut_v               (lut_v),
+      .d_x                 (d_u_n),
+      .d_y                 (d_v_n),
+      .x_in                (u_n),
+      .y_in                (v_n),
     // ----------------------------------
     // Data outputs
     // ----------------------------------
-      .u_np1               (u_np1),
-      .v_np1               (v_np1)
-    );
+      .x_out               (u_n_times_d_u_n),
+      .y_out               (v_n_times_d_v_n)
+   );
    // -----------------------------------------------------
 
+   // TODO: think of a better implementation for this
+   assign   u_n_plus_d_u_n =  d_u_n[`D_SIGN]    ?  u_n - d_u_n[`D_DATA] :  u_n + d_u_n[`D_DATA] ;
+   assign   v_n_plus_d_v_n =  d_v_n[`D_SIGN]    ?  v_n - d_v_n[`D_DATA] :  v_n + d_v_n[`D_DATA] ;
+   //assign   u_n_plus_d_u_n =  d_u_n != 2'b10    ?  u_n + d_u_n :  u_n   ;
+   //assign   v_n_plus_d_v_n =  d_v_n != 2'b10    ?  v_n + d_v_n :  v_n   ;
+
+   //       +--------------------------------------------------------------------+
+   //       | Mux the inputs of the two's complement complex adder               |
+   //       +--------------+--------------------+-----------+--------------------+
+   //       | Adder inputs |  mode              |  MODE_E   |  MODE_L            |
+   //       +--------------+--------------------+-----------+--------------------+
+   assign   sign_a_u       =  mode == `MODE_E   ?  `ADD     :  `ADD              ;
+   assign   sign_a_v       =  mode == `MODE_E   ?  `ADD     :  `ADD              ;
+   //assign sign_b_u       =  mode == `MODE_E   ?  `SUBB    :  d_u_n[`D_SIGN]    ;
+   //assign sign_b_v       =  mode == `MODE_E   ?  `SUBB    :  d_v_n[`D_SIGN]    ;
+   assign   sign_b_u       =  mode == `MODE_E   ?  `SUBB    :  `ADD              ;
+   assign   sign_b_v       =  mode == `MODE_E   ?  `SUBB    :  `ADD              ;
+   assign   a_u            =  mode == `MODE_E   ?   u_n     :   u_n_plus_d_u_n   ;
+   assign   a_v            =  mode == `MODE_E   ?   v_n     :   v_n_plus_d_v_n   ;
+   assign   b_u            =  mode == `MODE_E   ?   lut_u   :   u_n_times_d_u_n  ;
+   assign   b_v            =  mode == `MODE_E   ?   lut_v   :   v_n_times_d_v_n  ;
+   //       +--------------+--------------------+-----------+--------------------+
+
    // -----------------------------------------------------
-   // Data step
+   // Barrel shifter for u
    // -----------------------------------------------------
-   bkm_data_step #(
+   barrel_shifter #(
     // ----------------------------------
     // Parameters
     // ----------------------------------
-      .W                   (W),
-      .LOG2W               (LOG2W),
-      .LOG2N               (LOG2N),
-      .ARCH                ("2C")
-   ) bkm_data_step (
-   // ----------------------------------
-   // Clock, reset & enable inputs
-   // ----------------------------------
-      .clk                 (clk),
-      .arst                (arst),
-      .srst                (srst),
-      .enable              (enable),
+      .W                   (W/4),
+      .LOG2W               (LOG2W-2)
+   ) barrel_shifter_u (
     // ----------------------------------
     // Data inputs
     // ----------------------------------
-      .mode                (mode),
-      .format              (format),
-      .n                   (n),
-      .d_x_n               (d_x_n),
-      .d_y_n               (d_y_n),
-      .X_n                 (X_n),
-      .Y_n                 (Y_n),
-      .lut_X               (lut_X),
-      .lut_Y               (lut_Y),
+      .dir                 (`DIR_RIGHT),
+      .op                  (`OP_SHIFT),
+      .shift_t             (`SHIFT_ARITH),
+      .sel                 (n),
+      .in                  (b_u),
     // ----------------------------------
     // Data outputs
     // ----------------------------------
-      .X_np1               (X_np1),
-      .Y_np1               (Y_np1)
-    );
+      .out                 (b_u_shifted)
+   );
+   // -----------------------------------------------------
+
+   // -----------------------------------------------------
+   // Barrel shifter for v
+   // -----------------------------------------------------
+   barrel_shifter #(
+    // ----------------------------------
+    // Parameters
+    // ----------------------------------
+      .W                   (W/4),
+      .LOG2W               (LOG2W-2)
+   ) barrel_shifter_v (
+    // ----------------------------------
+    // Data inputs
+    // ----------------------------------
+      .dir                 (`DIR_RIGHT),
+      .op                  (`OP_SHIFT),
+      .shift_t             (`SHIFT_ARITH),
+      .sel                 (n),
+      .in                  (b_v),
+    // ----------------------------------
+    // Data outputs
+    // ----------------------------------
+      .out                 (b_v_shifted)
+   );
+   // -----------------------------------------------------
+
+   // -----------------------------------------------------
+   // Complex adder for w_n
+   // -----------------------------------------------------
+   complex_add_subb #(
+    // ----------------------------------
+    // Parameters
+    // ----------------------------------
+      .W                   (W/4)
+   ) complex_add_sub (
+    // ----------------------------------
+    // Data inputs
+    // ----------------------------------
+      .subb_a_x            (sign_a_u),
+      .subb_a_y            (sign_a_v),
+      .subb_b_x            (sign_b_u),
+      .subb_b_y            (sign_b_v),
+      .a_x                 (a_u),
+      .a_y                 (a_v),
+      .b_x                 (b_u_shifted),
+      .b_y                 (b_v_shifted),
+    // ----------------------------------
+    // Data outputs
+    // ----------------------------------
+      .c_x                 (c_u),
+      .c_y                 (c_v),
+      .s_x                 (s_u),
+      .s_y                 (s_v)
+   );
+   // -----------------------------------------------------
+
+   // -----------------------------------------------------
+   // Output assignment for w_n+1
+   // -----------------------------------------------------
+   // Multiply by 2 outputs
+   assign u_np1 = s_u << 1;
+   assign v_np1 = s_v << 1;
    // -----------------------------------------------------
 
 // *****************************************************************************
