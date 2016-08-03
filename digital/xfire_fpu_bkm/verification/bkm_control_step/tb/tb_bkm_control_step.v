cdb81058442cd842a359be08e9e36e47f4f97fbb
Ignacio Lesser <ilesser@users.noreply.github.com>
2016-08-02 21:13:28 -0300

    updated W parameter and added load for cnt

--- digital/xfire_fpu_bkm/verification/bkm_control_step/tb/tb_bkm_control_step.v
+++ digital/xfire_fpu_bkm/verification/bkm_control_step/tb/tb_bkm_control_step.v
@@ -24,21 +24,23 @@
 // History:
 // --------
 //
+//    - 2016-08-02 - ilesser - Changed the definition of W.
+//    - 2016-08-02 - ilesser - Added parallel load to the counter.
+//    - 2016-07-25 - ilesser - Added checker.
 //    - 2016-07-23 - ilesser - Initial version.
 //
 // -----------------------------------------------------------------------------
 
 `define SIM_CLK_PERIOD_NS 10
 `timescale 1ns/1ps
-`define W 64
-`define WC 16
-`define N 64
-`define LOG2W 6
-`define LOG2N 6
+`define N 16
+`define W 16
+`define LOG2W 4
+`define LOG2N 4
 `define M_SIZE 1
 `define F_SIZE 2
 `define D_SIZE 2
-`define CNT_SIZE `M_SIZE+`F_SIZE+`D_SIZE+`D_SIZE+`LOG2N+4*(`WC)
+`define CNT_SIZE `M_SIZE+`F_SIZE+`D_SIZE+`D_SIZE+`LOG2N+4*(`W)
 
 `include "/home/ilesser/simlib/simlib_defs.vh"
 
@@ -55,29 +57,28 @@ module tb_bkm_control_step ();
    // -----------------------------------------------------
    // Testbench controlled variables and signals
    // -----------------------------------------------------
-   localparam              W = `W;
    wire                    clk;
-   reg                     arst, srst, ena;
+   reg                     arst, srst, ena, load;
    reg                     tb_mode;
    reg   [1:0]             tb_format;
    reg   [`LOG2N-1:0]      tb_n;
    reg   [1:0]             tb_d_u_n;
    reg   [1:0]             tb_d_v_n;
-   reg   [W/4-1:0]         tb_u_n,     tb_v_n;
-   reg   [W/4-1:0]         tb_lut_u_n, tb_lut_v_n;
-   reg   [W/4-1:0]         tb_u_np1,   tb_v_np1;
-   reg   [`CNT_SIZE-1:0]   cnt;
+   reg   [`W-1:0]          tb_u_n,     tb_v_n;
+   reg   [`W-1:0]          tb_lut_u_n, tb_lut_v_n;
+   reg   [`W-1:0]          tb_u_np1,   tb_v_np1;
+   reg   [`CNT_SIZE-1:0]   cnt, cnt_load, cnt_step;
    // -----------------------------------------------------
 
    // -----------------------------------------------------
    // Testbecnch wiring
    // -----------------------------------------------------
-   wire  [W/4-1:0]         res_u_np1,  res_v_np1;
+   wire  [`W-1:0]          res_u_np1,  res_v_np1;
 
    // Checker wiring
    wire                    err_u,   err_v;
    wire                    war_u,   war_v;
-   wire  [W/4-1:0]         delta_u, delta_v;
+   wire  [`W-1:0]          delta_u, delta_v;
    // -----------------------------------------------------
 
    // -----------------------------------------------------
@@ -100,10 +101,14 @@ module tb_bkm_control_step ();
    );
 
    always @(posedge clk)
-       if (arst==1'b1) begin
-          cnt <= {`CNT_SIZE{1'b0}};
-       end else if (ena) begin
-          cnt <= cnt + 1;
+      if (arst==1'b1) begin
+         cnt <= {`CNT_SIZE{1'b0}};
+      end
+      else if (ena) begin
+         if (load)
+            cnt <= cnt_load;
+         else
+          cnt <= cnt + cnt_step;
        end
    // -----------------------------------------------------
 
@@ -111,17 +116,17 @@ module tb_bkm_control_step ();
    // Monitors
    // -----------------------------------------------------
    initial begin
-      $monitor("Time = %8t",                                               $time,
-               "\ttb_mode=%b",                                             tb_mode,
-               "\ttb_format=%b",                                           tb_format,
-               "\ttb_n=%b",                                                tb_n,
-               "\ttb_d_u_n=%b\ttb_d_v_n=%b\n",                             tb_d_u_n, tb_d_v_n,
-               "\ttb_u_n=%6d\ttb_v_n=%6d\t tb_u_np1=%6d\t tb_v_np1=%6d\n", tb_u_n, tb_v_n, tb_u_np1, tb_v_np1,
-               "\t\t\t\t\tres_u_np1=%6d\tres_v_np1=%6d\n",                                res_u_np1,res_v_np1,
-            );
+      //$monitor("Time = %8t",                                               $time,
+               //"\ttb_mode=%b",                                             tb_mode,
+               //"\ttb_format=%b",                                           tb_format,
+               //"\ttb_n=%b",                                                tb_n,
+               //"\ttb_d_u_n=%b\ttb_d_v_n=%b\n",                             tb_d_u_n, tb_d_v_n,
+               //"\ttb_u_n=%6d\ttb_v_n=%6d\t tb_u_np1=%6d\t tb_v_np1=%6d\n", tb_u_n, tb_v_n, tb_u_np1, tb_v_np1,
+               //"\t\t\t\t\tres_u_np1=%6d\tres_v_np1=%6d\n",                                res_u_np1,res_v_np1,
+            //);
 
-      $dumpfile("../waves/tb_bkm_control_step.vcd");
-      $dumpvars();
+      //$dumpfile("../waves/tb_bkm_control_step.vcd");
+      //$dumpvars();
    end
    // -----------------------------------------------------
 
