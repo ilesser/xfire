40cccfc0b41956532da9bc7172189047e2c1c475
ilesser <ilesser@users.noreply.github.com>
2016-09-04 20:34:01 -0300

    Implemented real number model of get_d block.

--- digital/xfire_fpu_bkm/verification/get_d/tb/tb_get_d.v
+++ digital/xfire_fpu_bkm/verification/get_d/tb/tb_get_d.v
@@ -24,6 +24,7 @@
 // History:
 // --------
 //
+//    - 2016-09-04 - ilesser - Added parameters for the integer part.
 //    - 2016-07-10 - ilesser - Changed architecture and updated limits.
 //    - 2016-05-17 - ilesser - Initial version.
 //
@@ -31,8 +32,12 @@
 
 `define SIM_CLK_PERIOD_NS 10
 `timescale 1ns/1ps
-`define W 8
-`define CNT_SIZE `W+1
+`define UGC  3
+`define LGC  1
+`define WI  11
+`define WC  `UGC + 64/4 + `LGC
+`define WF  `WC - `UGC - `WI
+`define CNT_SIZE 1+2*`WC
 
 `include "bkm_defs.vh"
 `include "/home/ilesser/simlib/simlib_defs.vh"
@@ -50,24 +55,22 @@ module tb_get_d ();
    // -----------------------------------------------------
    // Testbench controlled variables and signals
    // -----------------------------------------------------
-   localparam                    W = `W;
-   localparam                    CNT_SIZE = `CNT_SIZE;
-   wire                          clk;
-   reg                           rst, ena;
-   reg                           err_x, err_y;
-   reg            [CNT_SIZE-1:0] cnt;
-   reg                           tb_mode;
-   reg   signed   [W-1:0]        tb_u;
-   reg   signed   [W-1:0]        tb_v;
-   reg   signed   [1:0]          tb_d_x;
-   reg   signed   [1:0]          tb_d_y;
+   reg                              rst, ena;
+   reg                              err_x, err_y;
+   reg            [`CNT_SIZE-1:0]   cnt;
+   reg                              tb_mode;
+   reg   signed   [`WC-1:0]         tb_u;
+   reg   signed   [`WC-1:0]         tb_v;
+   reg   signed   [1:0]             tb_d_x;
+   reg   signed   [1:0]             tb_d_y;
    // -----------------------------------------------------
 
    // -----------------------------------------------------
    // Testbench wiring
    // -----------------------------------------------------
-   wire  signed   [1:0]          res_d_x;
-   wire  signed   [1:0]          res_d_y;
+   wire                             clk;
+   wire  signed   [1:0]             res_d_x;
+   wire  signed   [1:0]             res_d_y;
    // -----------------------------------------------------
 
    // -----------------------------------------------------
@@ -101,7 +104,9 @@ module tb_get_d ();
    // Monitors
    // -----------------------------------------------------
    initial begin
-      $monitor("Time = %8t tb_u = %d tb_v = %d tb_d_x = %d tb_d_y = %d res_d_x = %d res_d_y = %d\n",$time, tb_u, tb_v, tb_d_x, tb_d_y, res_d_x, res_d_y);
+      `ifdef RTL_DEBUG
+         $monitor("Time = %8t tb_u = %d tb_v = %d tb_d_x = %d tb_d_y = %d res_d_x = %d res_d_y = %d\n",$time, tb_u, tb_v, tb_d_x, tb_d_y, res_d_x, res_d_y);
+      `endif
       $dumpfile("../waves/tb_get_d_n.vcd");
       $dumpvars();
    end
@@ -112,11 +117,10 @@ module tb_get_d ();
    // -----------------------------------------------------
    always @(posedge clk) begin
       if (ena == 1'b1) begin
-         if (tb_d_x != res_d_x) begin
+         if (tb_d_x !== res_d_x) begin
             $display("[%0d] ERROR: Different d_x! Expected result: %d \n\t\t\t\t Obtained result: %d\t\t. Instance: %m",$time, tb_d_x, res_d_x);
             add_error();
             err_x = 1'b1;
-            //finish_sim();
          end
          else
             err_x = 1'b0;
@@ -125,11 +129,10 @@ module tb_get_d ();
 
    always @(posedge clk) begin
       if (ena == 1'b1) begin
-         if (tb_d_y != res_d_y) begin
+         if (tb_d_y !== res_d_y) begin
             $display("[%0d] ERROR: Different d_y! Expected result: %d \n\t\t\t\t Obtained result: %d\t\t. Instance: %m",$time, tb_d_y, res_d_y);
             add_error();
             err_y = 1'b1;
-            //finish_sim();
          end
          else
             err_y = 1'b0;
@@ -141,7 +144,10 @@ module tb_get_d ();
    // Device under verifiacion
    // -----------------------------------------------------
    get_d #(
-      .W(W)
+      .WC      (`WC),
+      .UGC     (`UGC),
+      .LGC     (`LGC),
+      .WI      (`WI)
    ) duv (
       .mode    (tb_mode),
       .u       (tb_u),
