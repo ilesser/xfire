0b46bd5b2e08b8c32bc2c97d80391ca8f1f69e5e
Ignacio Lesser <nachoyo89@gmail.com>
2016-09-05 19:23:20 -0300

    Changed bkm_step_task to have real type inputs.

--- digital/xfire_fpu_bkm/verification/bkm_step/tests/bkm_step_task.v
+++ digital/xfire_fpu_bkm/verification/bkm_step/tests/bkm_step_task.v
@@ -24,6 +24,7 @@
 // History:
 // --------
 //
+//    - 2016-09-05 - ilesser - Changed bkm_step_task inputs to real type.
 //    - 2016-08-15 - ilesser - Initial version.
 //
 // -----------------------------------------------------------------------------
@@ -43,23 +44,23 @@ task bkm_step;
    input    [`LOG2N-1:0]   n;
    input    [1:0]          d_x_n;
    input    [1:0]          d_y_n;
-   input    [`WD-1:0]      X_n_bin;
-   input    [`WD-1:0]      Y_n_bin;
-   input    [`WC-1:0]      u_n_bin;
-   input    [`WC-1:0]      v_n_bin;
-   input    [`WD-1:0]      lut_X_n_bin;
-   input    [`WD-1:0]      lut_Y_n_bin;
-   input    [`WC-1:0]      lut_u_n_bin;
-   input    [`WC-1:0]      lut_v_n_bin;
+   input    real           X_n;
+   input    real           Y_n;
+   input    real           u_n;
+   input    real           v_n;
+   input    real           lut_X_n;
+   input    real           lut_Y_n;
+   input    real           lut_u_n;
+   input    real           lut_v_n;
    // ----------------------------------
 
    // ----------------------------------
    // Data outputs
    // ----------------------------------
-   output   [`WD-1:0]      X_np1_bin;
-   output   [`WD-1:0]      Y_np1_bin;
-   output   [`WC-1:0]      u_np1_bin;
-   output   [`WC-1:0]      v_np1_bin;
+   output   real           X_np1;
+   output   real           Y_np1;
+   output   real           u_np1;
+   output   real           v_np1;
    // ----------------------------------
 
 // *****************************************************************************
@@ -73,35 +74,19 @@ task bkm_step;
    // -----------------------------------------------------
    reg   double_word;
    reg   complex;
-   real  nc, nd;
    real  dx,                        dy;
-   real  X_n,                       Y_n;
    real  X_n_times_d_n_r,           Y_n_times_d_n_r;
    real  X_n_times_d_n_div_2_n_r,   Y_n_times_d_n_div_2_n_r;
-   real  X_np1,                     Y_np1;
-   real  lut_X_n,                   lut_Y_n;
    real  du,                        dv;
-   real  u_n,                       v_n;
-   real  u_np1,                     v_np1;
-   real  lut_u_n,                   lut_v_n;
    real  u_n_plus_d_u_n_r,          v_n_plus_d_v_n_r;
    real  u_n_times_d_n_r,           v_n_times_d_n_r;
    real  u_n_times_d_n_div_2_n_r,   v_n_times_d_n_div_2_n_r;
-
-   real  u_np11,                     v_np11;
-   real a,b,c,d,e,f;
-   integer ee,ff;
    // -----------------------------------------------------
 
    begin
 
       double_word = format[0];
       complex     = format[1];
-
-      // Calculate n values
-      nd = $itor(n);
-      //nc = $itor(n[`LOG2N+`LOG2WC-`LOG2WD-1:0]);
-      nc = $itor(n);
 
       dx =  d_x_n == 2'b01 ?  1  :
             d_x_n == 2'b11 ? -1  :
@@ -113,27 +98,6 @@ task bkm_step;
 
       du = dx;
       dv = dy;
-
-      if (double_word==1'b1) begin
-         X_n      = $itor($signed(X_n_bin));
-         Y_n      = $itor($signed(Y_n_bin));
-         lut_X_n  = $itor($signed(lut_X_n_bin));
-         lut_Y_n  = $itor($signed(lut_Y_n_bin));
-         u_n      = $itor($signed(u_n_bin));
-         v_n      = $itor($signed(v_n_bin));
-         lut_u_n  = $itor($signed(lut_u_n_bin));
-         lut_v_n  = $itor($signed(lut_v_n_bin));
-      end
-      else begin
-         X_n      = $itor($signed(X_n_bin     [`WD/2-1:0]));
-         Y_n      = $itor($signed(Y_n_bin     [`WD/2-1:0]));
-         lut_X_n  = $itor($signed(lut_X_n_bin [`WD/2-1:0]));
-         lut_Y_n  = $itor($signed(lut_Y_n_bin [`WD/2-1:0]));
-         u_n      = $itor($signed(u_n_bin     [`WC/2-1:0]));
-         v_n      = $itor($signed(v_n_bin     [`WC/2-1:0]));
-         lut_u_n  = $itor($signed(lut_u_n_bin [`WC/2-1:0]));
-         lut_v_n  = $itor($signed(lut_v_n_bin [`WC/2-1:0]));
-      end // if (double_word==1'b1)
 
       if (mode==`MODE_E) begin
       // E-mode
@@ -225,11 +189,6 @@ task bkm_step;
          end
 
       end // if (mode==`MODE_E) begin
-
-      X_np1_bin = X_np1;
-      Y_np1_bin = Y_np1;
-      u_np1_bin = u_np1;
-      v_np1_bin = v_np1;
 
    end
 
